40030066f9d9fbadbc5cd3bed76f63f6e3210a84
Ali Elbashir <alielbashiir@gmail.com>
2021-12-21 18:55:02 +0300

    Change model fields to optional

--- lib/model.dart
+++ lib/model.dart
@@ -1,40 +1,59 @@
-enum EducationType{
-  birinciOgretim,
-  ikinciOgretim
-}
+enum EducationType { birinciOgretim, ikinciOgretim }
 
 class Project {
-   Personnel? advisor; // danisman
-   List<Personnel>? jury; // juri
-   List<Student>? authors;
-   User? uploader; // yukleyen
-   String? course; // ders [arastirma problemleri, bitirme projesi]
-   String? title; // baslik
-   String? summary; // ozet
-   String? submissionDate; // donem
-   List<String>? keywords;
+  Personnel? advisor; // danisman
+  List<Personnel>? jury; // juri
+  List<Student>? authors;
+  User? uploader; // yukleyen
+  String? course; // ders [arastirma problemleri, bitirme projesi]
+  String? title; // baslik
+  String? summary; // ozet
+  String? submissionDate; // donem
+  List<String>? keywords;
+
+  Project(
+      {this.advisor,
+        this.jury,
+        this.authors,
+        this.uploader,
+        this.course,
+        this.title,
+        this.summary,
+        this.submissionDate,
+        this.keywords});
 }
 
 class Person {
-    String? firstName; // ad
-  String? lastName; // soyad
+  String? firstName; // ad
+  String? lastName;
+
+  Person({this.firstName, this.lastName}); // soyad
+
 }
 
 class Student extends Person {
   int? id; // ogrenci numarasi
   EducationType? educationType;
+
+  Student({this.id, this.educationType, String? firstName, String? lastName})
+      : super(firstName: firstName, lastName: lastName);
 }
 
-
-
 class User {
-   String? name;
-   String? email;
-   String? password;
-   String? privilege;
-   bool? deleted;
+  String? name;
+  String? email;
+  String? password;
+  String? privilege;
+  bool? deleted;
+
+  User({this.name, this.email, this.password, this.privilege, this.deleted});
 }
 
 class Personnel extends Person {
- String? title; // unvan
+  String? id;
+  String? title;
+
+  Personnel({this.id, this.title, String? firstName, String? lastName})
+      : super(firstName: firstName, lastName: lastName); // unvan
+
 }
