66cf3d4d039688a3194d2221dcc81ff906954fbb
Ali Elbashir <alielbashiir@gmail.com>
2021-12-21 23:09:25 +0300

    Add createuser Fix getUsers to not get deleted Remove insertUser

--- lib/util/database.dart
+++ lib/util/database.dart
@@ -46,3 +46,87 @@ Future<User> createUser(User user, PostgrestClient db) async {
   user.id = response.data[0]['id'];
   return user;
 }
+
+/// Returns list of projects with the limits specified in [query]
+Future<List> getProjects(PostgrestClient db, Project project, User user) async {
+  const selectString =
+      "id, title, course, keywords:project_keyword!inner(keyword) ,summary, submission_date, authors:student!inner(*), advisor:project_personnel_id_fk!inner(title, first_name, last_name), jury:personnel!project_jury!inner(title, first_name, last_name)";
+
+  var query = db.from('project').select(selectString);
+
+  // only return user's own uploaded projects
+  if (user.privilege == 'user') {
+    query.eq('uploader_id', user.id);
+  }
+
+  // all projects with ali elbashir as author
+  // all projects with ali elbashir and rammah ali as authors
+  // authors filter by id
+  if (project.authors != null && project.authors!.isNotEmpty) {
+    var authors = '(';
+    for (var author in project.authors!) {
+      authors += '${author.id},';
+    }
+    authors = authors.substring(0, authors.length - 1);
+    authors += ')';
+
+    query.filter('authors.id', 'in', authors);
+  }
+
+  // all projects with david malan as advisor
+  // advisor filter by id
+  if (project.advisor != null) {
+    query.filter('advisors.id', 'eq', project.advisor);
+  }
+
+  // all projects with yeni haberlesme protokolu as title
+  // title filter by name
+  if (project.title != null) {
+    query.eq('title', project.title);
+  }
+
+  // all projects with arastirma problemleri as course
+  // course filter by name
+  if (project.course != null) {
+    query.filter('course', 'eq', 'arastirma problemleri');
+  }
+
+  // all projects with david malan in the jury
+  // jury filter by id
+  if (project.jury != null && project.jury!.isNotEmpty) {
+    var jury = '(';
+    for (var personnel in project.jury!) {
+      jury += '"${personnel.id}",';
+    }
+    jury = jury.substring(0, jury.length - 1);
+    jury += ')';
+
+    query.filter('jury.id', 'in', jury);
+  }
+
+  // all projects with submission date guz-2018-2019
+  if (project.submissionDate != null) {
+    query.filter('submission_date', 'eq', project.submissionDate);
+  }
+
+  // all projects with keywords http
+  if (project.keywords != null && project.keywords!.isNotEmpty) {
+    var keywords = '(';
+    for (var keyword in project.keywords!) {
+      keywords += '"${keyword}",';
+    }
+    keywords = keywords.substring(0, keywords.length - 1);
+    keywords += ')';
+
+    query.filter('keywords.keyword', 'in', keywords);
+  }
+
+  final response = await query.execute();
+
+  if (response.hasError) {
+    throw Exception(response.error);
+  }
+
+  return response.data;
+}
+
